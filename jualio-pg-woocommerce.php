a791fe3259a5699b78e1997880d8fffd101714b2
Yuzar Pratama <yuzarpratama@gmail.com>
2015-06-22 16:58:47 +0700

    add tutorial jualio payment

--- jualio-pg-woocommerce.php
+++ jualio-pg-woocommerce.php
@@ -7,6 +7,25 @@ Version: 1.0
 Author: Yuzar
 Author URI: http://www.saklik.com
 */
+add_action('admin_menu', 'jl_pg_plugin_setup_menu');
+ 
+function jl_pg_plugin_setup_menu(){
+    add_menu_page( 'Jualio Payment Tutorial', 'Jualio Payment Tutorial', 'manage_options', 'jl-pg-Tutorial', 'jl_pg_init' );
+
+}
+
+function jl_pg_init(){
+    include_once('template/tutorial-pg.php');
+}
+
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'my_plugin_action_links' );
+
+function my_plugin_action_links( $links ) {
+   $links[] = '<a href="'. esc_url( get_admin_url(null, 'admin.php?page=jl-pg-Tutorial') ) .'">Tutorial</a>';
+   return $links;
+}
+
+
 add_action('plugins_loaded', 'woocommerce_jualio_pg_init', 0);
 
 
